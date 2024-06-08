d0826cbd50512a462d4d83c6b7e0d80c5a971d55
Rahul1998sys <54656739+Rahul1998sys@users.noreply.github.com>
2024-06-08 09:01:46 +0530

    Web.js

---
+++ Web.js
@@ -0,0 +1,10 @@
+alert("For good experience , Please make sure that you should wait  for 3 to 5 seconds at least Because image and other animation will take time to reload 🤝💗 ") ;
+
+alert(" Basically in this small webpage I didn't add nav bar but I will,  it's  pending 👾💗 ") ;
+
+alert(" Hope you  guys will love it😍🫠🙌🏻🫰🏻 ") ;
+
+alert("I think it's almost Responsive🌚🌸");
+
+alert("Feel free to Like,comment and follow🤩 more codes will definitely come ") ;
+alert(" Keep learning ✅🫶🔥💝") ;
